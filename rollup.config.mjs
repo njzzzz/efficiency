d6f69f6c6ae9f3260bdf6096eb0fc531a07aa15f
nijr <nijr@citycloud.com.cn>
2023-03-29 10:19:39 +0800

    chore: 切换至pnpm

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,7 +3,7 @@ import babel from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
 import nodeResolve from "@rollup/plugin-node-resolve";
 import vue from "rollup-plugin-vue";
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 import pkg from "./package.json" assert { type: "json" };
 
